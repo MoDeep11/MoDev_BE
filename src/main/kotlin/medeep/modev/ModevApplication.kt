ffd82513b851da455ccb782afa0f10cb5c4a402d
kusuri12 <waterfail12@naver.com>
2026-05-30 22:17:34 +0900

    first commit

---
+++ src/main/kotlin/medeep/modev/ModevApplication.kt
@@ -0,0 +1,11 @@
+package medeep.modev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModevApplication
+
+fun main(args: Array<String>) {
+    runApplication<ModevApplication>(*args)
+}
---
+++ src/test/kotlin/medeep/modev/ModevApplicationTests.kt
@@ -0,0 +1,13 @@
+package medeep.modev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
