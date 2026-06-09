95ac48b16873914d56dcbccb54099e2235869852
곽효오 <spec3290@gmail.com>
2026-06-09 20:24:46 +0900

    feat(project) : 프로젝트 의존성 id dto 추가

---
+++ src/main/kotlin/modeep/modev/domain/project/entity/id/ProjectDependencyId.kt
@@ -0,0 +1,13 @@
+package modeep.modev.domain.project.entity.id
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import java.io.Serializable
+
+@Embeddable
+data class ProjectDependencyId(
+    @Column(name = "project_id", length = 50)
+    val projectId: String = "",
+    @Column(name = "dependency_id")
+    val dependencyId: Long = 0L,
+) : Serializable
