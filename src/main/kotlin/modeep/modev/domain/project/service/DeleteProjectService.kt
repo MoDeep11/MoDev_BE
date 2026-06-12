d12d3d4e4ab05ebbc212c45d66ab08d4abfa3d4b
곽효오 <spec3290@gmail.com>
2026-06-12 10:19:29 +0900

    refactor(project) : 프로젝트 서비스 파일 작업별로 분류
    projectservice -> (delete, get, patch, post)service

---
+++ src/main/kotlin/modeep/modev/domain/project/service/DeleteProjectService.kt
@@ -0,0 +1,35 @@
+package modeep.modev.domain.project.service
+
+import modeep.modev.domain.project.controller.dto.response.DeleteProjectResponse
+import modeep.modev.domain.project.repository.ProjectRepository
+import modeep.modev.global.exception.BusinessException
+import modeep.modev.global.exception.error.ProjectErrorCode
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+@Service
+class DeleteProjectService(
+    private val projectRepository: ProjectRepository,
+) {
+    @Transactional
+    fun deleteProject(projectId: String): DeleteProjectResponse {
+        val project =
+            projectRepository
+                .findByIdAndDeletedAtIsNull(projectId)
+                ?: throw BusinessException(ProjectErrorCode.PROJECT_NOT_FOUND)
+
+        val deletedAt = LocalDateTime.now()
+        val hardDeleteScheduledAt = deletedAt.plus(30, ChronoUnit.DAYS)
+        project.delete(
+            deletedAt = deletedAt,
+        )
+
+        return DeleteProjectResponse(
+            projectId = project.id,
+            deletedAt = deletedAt,
+            hardDeleteScheduledAt = hardDeleteScheduledAt,
+        )
+    }
+}
