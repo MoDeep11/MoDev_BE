bdaf0816df9fa11e3b0d8e4323d6a87dece12fa2
곽효오 <spec3290@gmail.com>
2026-06-17 00:25:31 +0900

    feat(auth) : add filterchain

---
+++ src/main/kotlin/modeep/modev/global/security/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package modeep.modev.global.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/auth/signup", "/auth/login")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+
+        return http.build()
+    }
+}
