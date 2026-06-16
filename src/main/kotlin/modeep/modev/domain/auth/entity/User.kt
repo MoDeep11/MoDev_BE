94073fe701bc4f1bc541b4c98777ff5ad9b4e172
곽효오 <spec3290@gmail.com>
2026-06-17 01:08:09 +0900

    feat(user) : id 생성 타입 uuid로 변경

--- src/main/kotlin/modeep/modev/domain/auth/entity/User.kt
+++ src/main/kotlin/modeep/modev/domain/auth/entity/User.kt
@@ -4,18 +4,17 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import modeep.modev.global.common.BaseEntity
+import org.hibernate.annotations.UuidGenerator
 import java.time.Instant
 
 @Entity
 @Table(name = "users")
 class User(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
+    @Id @UuidGenerator
+    val id: String,
     @Column(name = "public_id", unique = true, nullable = false, updatable = false, length = 100)
     val publicId: String,
     @Column(unique = true, nullable = false, length = 320)
