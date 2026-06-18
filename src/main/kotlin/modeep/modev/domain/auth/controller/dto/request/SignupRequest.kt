e410a1373417bb08ac526bb40400c305ffbe9e96
곽효오 <spec3290@gmail.com>
2026-06-18 23:54:08 +0900

    fix(auth) : @email, @notblank 어노테이션 적용

--- src/main/kotlin/modeep/modev/domain/auth/controller/dto/request/SignupRequest.kt
+++ src/main/kotlin/modeep/modev/domain/auth/controller/dto/request/SignupRequest.kt
@@ -1,7 +1,12 @@
 package modeep.modev.domain.auth.controller.dto.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
 data class SignupRequest(
+    @Email
     val email: String,
+    @NotBlank
     val password: String,
     val passwordConfirm: String,
 )
