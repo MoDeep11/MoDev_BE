243ae55530df5f8b37537c8b2a09913d979c6392
곽효오 <spec3290@gmail.com>
2026-06-23 00:27:10 +0900

    chore(auth) : 파일 구조 변경에 따른 임포트문 제거

--- src/main/kotlin/modeep/modev/domain/user/entity/User.kt
+++ src/main/kotlin/modeep/modev/domain/user/entity/User.kt
@@ -8,7 +8,6 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import modeep.modev.domain.auth.entity.UserStatus
 import modeep.modev.global.common.BaseEntity
 import java.time.Instant
 
