c85f333ccedf350d7c592deab490c6c90bc66f32
kusuri12 <waterfail12@naver.com>
2026-07-04 21:08:24 +0900

    refactor(structure): 프로젝트 기술 스택 수정 API 응답 DTO의 status를 enum을 사용하도록 변경

--- src/main/kotlin/modeep/modev/domain/project/controller/dto/response/UpdateProjectStacksResponse.kt
+++ src/main/kotlin/modeep/modev/domain/project/controller/dto/response/UpdateProjectStacksResponse.kt
@@ -1,8 +1,9 @@
 package modeep.modev.domain.project.controller.dto.response
 
+import modeep.modev.domain.project.entity.ProjectStatus
 import java.util.UUID
 
 data class UpdateProjectStacksResponse(
     val projectId: UUID,
-    val status: String,
+    val status: ProjectStatus,
 )
